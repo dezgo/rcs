c7ae7fef5633b8cdd7316245862c6f78402d7fb5
Derek Gillett <github@derekgillett.com>
2016-06-05 03:05:15 +0000

    revert welcome page post auth scaffolding

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,17 +1,53 @@
-@extends('layouts.app')
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Laravel</title>
 
-@section('content')
-<div class="container">
-    <div class="row">
-        <div class="col-md-10 col-md-offset-1">
-            <div class="panel panel-default">
-                <div class="panel-heading">Welcome</div>
+        <link href="https://fonts.googleapis.com/css?family=Lato:100" rel="stylesheet" type="text/css">
 
-                <div class="panel-body">
-                    Your Application's Landing Page.
-                </div>
+        <style>
+            html, body {
+                height: 100%;
+            }
+
+            body {
+                margin: 0;
+                padding: 0;
+                width: 100%;
+                display: table;
+                font-weight: 100;
+                font-family: 'Lato';
+            }
+
+            .container {
+                text-align: center;
+                display: table-cell;
+                vertical-align: middle;
+            }
+
+            .content {
+                text-align: center;
+                display: inline-block;
+            }
+
+            .title {
+                font-size: 96px;
+            }
+            .contact {
+	            font-family: 'Helvetica Neue', Helvetica, Arial, sans-serif;
+                font-size: 26px;
+            }
+        </style>
+    </head>
+    <body>
+        <div class="container">
+            <div class="content">
+                <div class="title">Aameen Rendering &amp; Construction</div>
+                <br>
+                <div class="contact">
+                Nabeel Allkoud - 0430 003 434
+            </div>
             </div>
         </div>
-    </div>
-</div>
-@endsection
+    </body>
+</html>
