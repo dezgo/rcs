990d9c59e58c97d4b1c661389e0af5fec5a64725
Derek Gillett <github@derekgillett.com>
2016-06-05 01:32:19 +0000

    add auth scaffolding

---
+++ resources/views/content/home.blade.php
@@ -0,0 +1,21 @@
+@extends('layouts.app')
+
+@section('head')
+<style>
+    body {
+        background-color: black;
+    }
+</style>
+@stop
+
+@section('content')
+<div class="container">
+    <div class="row">
+        <div class="col-md-6">
+            <img src="/images/logo.png">
+        </div>
+        <div class="col-md-6">
+        </div>
+    </div>
+</div>
+@endsection
